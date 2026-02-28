15b8173998c21f704d06aca89b967ffe52897ef2
erygle <eray.guler@std.yildiz.edu.tr>
2026-02-28 12:55:51 +0300

    update

--- sync.py
+++ sync.py
@@ -2,6 +2,7 @@ import os
 import requests
 import time
 from datetime import datetime
+from datetime import timedelta
 from dotenv import load_dotenv
 from ytmusicapi import YTMusic
 
@@ -10,7 +11,7 @@ load_dotenv()
 LASTFM_API_KEY = os.getenv("LASTFM_API_KEY")
 LASTFM_USER = os.getenv("LASTFM_USER")
 YT_PLAYLIST_ID = os.getenv("YT_PLAYLIST_ID")
-START_DATE = datetime(2026, 2, 23) # Başlangıç tarihi
+START_DATE = datetime.now() - timedelta(days=5) # Başlangıç tarihi
 
 def get_lastfm_tracks():
     if not LASTFM_API_KEY or not LASTFM_USER:
